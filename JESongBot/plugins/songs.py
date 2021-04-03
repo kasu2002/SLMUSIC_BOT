1fe3257bb1c2c01d2ac1c175b0771412b31071b1
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-03 12:15:31 +0530

    Update songs.py

--- JESongBot/plugins/songs.py
+++ JESongBot/plugins/songs.py
@@ -6,7 +6,7 @@ import requests
 import aiohttp
 import youtube_dl
 
-from pyrogram import filters
+from pyrogram import filters, Client
 from youtube_search import YoutubeSearch
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
 
